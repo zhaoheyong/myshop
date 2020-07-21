3878ae1a9830f104cc7f5a5c50138426da01ec27
zhaoheyong <zhaoheyong1992@163.com>
2020-07-21 10:50:06 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,17 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(VueRouter)
+Vue.use(ElementUI);
+Vue.use(Router)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
-
-export default router
